8f02cca381e14372d256ac38363d8bacfa3efc34
Shuhei Hiya <syuhei176@gmail.com>
2015-07-21 04:23:56 +0900

    Updated

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
 'use strict';
 
-// アプリケーションをコントロールするモジュール
 var app = require('app');
-// ウィンドウを作成するモジュール
 var BrowserWindow = require('browser-window');
 
-var MilkCocoa = require('milkcocoa');
-
-// クラッシュレポート
 require('crash-reporter').start();
 
-// メインウィンドウはGCされないようにグローバル宣言
-var mainWindow = null;
+var mainWin = null;
 
 // 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
     app.quit();
-  }
 });
 
-// Electronの初期化完了後に実行
+// Electronの初期化を待つ
 app.on('ready', function() {
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWin = new BrowserWindow({width: 800, height: 600});
+  mainWin.loadUrl('file://' + __dirname + '/index.html');
 
   // ウィンドウが閉じられたらアプリも終了
-  mainWindow.on('closed', function() {
-    mainWindow = null;
+  mainWin.on('closed', function() {
+    mainWin = null;
   });
 });
